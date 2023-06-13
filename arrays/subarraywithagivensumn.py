ea5948cede8c944c40c51517248ecfeee2de4d55
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-06-13 23:55:23 +0530

    Dsa Array

--- arrays/subarraywithagivensumn.py
+++ arrays/subarraywithagivensumn.py
@@ -1,20 +1,29 @@
-def sub(l,sum1):
-    curr = 0
+def subarray_with_sum(l, target_sum):
+    current_sum = 0
     start = 0
-    a=[]
+    result = []
+
     for i in range(len(l)):
-        curr += l[i]
-        while (curr>sum1) :
-            curr -= l[start]
-            start+=1
-        if (curr == sum1): 
+        current_sum += l[i]
+
+        # If the current sum exceeds the target sum, move the start index
+        # and subtract the element at start index from the current sum
+        while current_sum > target_sum:
+            current_sum -= l[start]
+            start += 1
+
+        # If the current sum matches the target sum, return True
+        if current_sum == target_sum:
             return True
-    if sum==0 or len(a)==0:
-        return -1
+
+    # If no subarray is found, return -1
+    return -1
+
 l = []
-n = int(input())
-for i in range(0, n):
-    f = int(input())
-    l.append(f)
-sum1 = int(input())
-print(sub(l,sum1))
+n = int(input("Enter the number of elements: "))
+for i in range(n):
+    element = int(input(f"Enter element {i + 1}: "))
+    l.append(element)
+
+target_sum = int(input("Enter the target sum: "))
+print(subarray_with_sum(l, target_sum))
