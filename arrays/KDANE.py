992c225f408edc8e4a69b7bc3f2932e2c45b5f7b
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-06-17 23:54:41 +0530

    KDANE.py

--- arrays/KDANE.py
+++ arrays/KDANE.py
@@ -3,12 +3,25 @@ def subar(l):
     curr_sum = 0 
     maX_sum = l[0]
     for i in range(len(l)):
-        curr_sum += l[i]
-        if curr_sum > maX_sum:           #(maximum in two of them )
-            maX_sum = curr_sum
-        if curr_sum <0:     #{ basically excluding negative part }
-            curr_sum = 0
+        curr_sum = max(curr_sum+l[i], l[i])
+        maX_sum = max(maX_sum , curr_sum)
     return maX_sum
+
+
+
+
+
+    # def maxSubArray(self, l: List[int]) -> int:
+    #     cur_sum= 0
+    #     max_sum= l[0]
+    #     for i in range(len(l)):
+    #         cur_sum += l[i]
+    #         if cur_sum>max_sum:
+    #             max_sum = cur_sum
+    #         if cur_sum<0:
+    #             cur_sum=0
+    #     return max_sum
+
 
 l = []
 n = int(input())
