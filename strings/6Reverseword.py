e59637c6aac9acac607ae5527e538e2d0e68ea91
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-06-20 23:58:39 +0530

    initial commit

--- strings/6Reverseword.py
+++ strings/6Reverseword.py
@@ -1,23 +1,30 @@
 def reverse_word(s, start, end):
     while start < end:
-        s[start], s[end]=s[end], s[start]
+        # Swap characters at start and end indices
+        s[start], s[end] = s[end], s[start]
+        # Move start index forward
         start = start + 1
+        # Move end index backward
         end -= 1
 
-s =input()
-s = list(s)
-start = 0
-while True:
+s = input()  # Take input from the user
+s = list(s)  # Convert input string to a list of characters
+start = 0  # Start index for word reversal
 
+while True:
     try:
+        # Find the index of the next space character (' ') starting from 'start'
         end = s.index(' ', start)
+        # Reverse the word between 'start' and 'end - 1'
         reverse_word(s, start, end - 1)
+        # Move 'start' index to the next word
         start = end + 1
 
-    except ValueError: # reverse last word
+    except ValueError:  # If no more spaces are found, reverse the last word
+        # Reverse the last word from 'start' to the end of the list
         reverse_word(s, start, len(s) - 1)
         break
 
-s.reverse()  #whole reverse string
-s = "".join(s)
-print(s)
+s.reverse()  # Reverse the entire list of characters
+s = "".join(s)  # Convert the list of characters back to a string
+print(s)  # Print the final reversed string
