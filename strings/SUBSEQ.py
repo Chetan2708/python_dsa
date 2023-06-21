a5db08dcc4b295d9b9ae1c7a34b4bd3426727a8c
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-06-21 23:51:20 +0530

    initial commit

--- strings/SUBSEQ.py
+++ strings/SUBSEQ.py
@@ -1,6 +1,6 @@
 def sub(s1,s2):
         i ,j = 0,0
-        while (i < len(s1) and j < len(s2)):
+        while (i < len(s1)  and j < len(s2)):
             if s1[i] == s2[j]:
                 j += 1
             i +=1
