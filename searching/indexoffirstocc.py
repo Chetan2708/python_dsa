899c3024482d02a3da9c800c11f6facbbae0938b
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-06-22 23:57:19 +0530

    initial commit

--- searching/indexoffirstocc.py
+++ searching/indexoffirstocc.py
@@ -4,23 +4,22 @@
 #             return(i)
 #     return -1
 def fun(l):
-    low =0
-    high = len(l) - 1  
-    index =-1        #[5,10,10,20,20]    
+    low = 0
+    high = len(l) - 1
+    index = -1  # Initialize the index as -1
     while low <= high:
-        mid = low+(high-low)//2
+        mid = low + (high - low) // 2  # Calculate the middle index
         print(mid)
-        if l[mid]==x:
-            index=mid
-            high=mid-1
-        if x>l[mid]:
-            low=mid+1
-        elif x<l[mid]:
-            high = mid -1
+        if l[mid] == x:  # If the middle element is equal to the target value
+            index = mid  # Update the index to the middle index
+            high = mid - 1  # Adjust the high pointer to search for the leftmost occurrence
+        if x > l[mid]:  # If the target value is greater than the middle element
+            low = mid + 1  # Adjust the low pointer to search in the right half
+        elif x < l[mid]:  # If the target value is less than the middle element
+            high = mid - 1  # Adjust the high pointer to search in the left half
     return index
 
-             
-#sorted array
-l= [5,10,10,10,20,20]
-x=10
+# Sorted array
+l = [5, 10, 10, 10, 20, 20]
+x = 10
 print(fun(l))
