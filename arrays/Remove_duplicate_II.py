d3d7b9252df5de5f45183658d272ed9f71e80484
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-06-26 23:58:36 +0530

    initial commit

---
+++ arrays/Remove_duplicate_II.py
@@ -0,0 +1,19 @@
+def fun (l):    
+    count = 1 
+    res = 1
+    for i in range(1, len(l)):
+        if l[i] == l[i-1]:
+            count+=1
+        else:
+            count =1
+        if count<=2:
+            l[res] =l[i]
+            res+=1 
+    return res
+
+
+
+l = [1,1,1,2,2,3,3,3,3,3,3,3,3,3]
+res = fun(l)
+for i in range(res):
+    print(l[i],end="")
