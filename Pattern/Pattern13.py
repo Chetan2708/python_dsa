4a72eb8e40ee527e2f9916c9fe4eaa0492a2d72a
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-07-04 23:32:17 +0530

    initial commit

---
+++ Pattern/Pattern13.py
@@ -0,0 +1,15 @@
+n = 5 
+num =1
+for i in range (n):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for k in range(2*i+1):
+        print(k+1,end=" ")
+    print() 
+
+
+#         1 
+#       1 2 3 
+#     1 2 3 4 5 
+#   1 2 3 4 5 6 7 
+# 1 2 3 4 5 6 7 8 9 
