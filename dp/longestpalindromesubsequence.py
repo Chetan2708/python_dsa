72c3e46ec653f071e29be03c2a3c5061b53e3f4d
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-08-16 23:49:21 +0530

    Initial commit

---
+++ dp/longestpalindromesubsequence.py
@@ -0,0 +1,17 @@
+def lcs(X, m, n):
+    Y = X[::-1]
+    L = [[None]*(n+1) for i in range(m+1)]
+ 
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                L[i][j] = L[i-1][j-1]+1
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+
+    return L[m][n]
+
+a = 'bbbab'
+print(lcs(a, len(a),len(a)))
