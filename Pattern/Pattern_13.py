db665c360c9934004fbb0fac49902a8c4c8731fe
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-09-10 23:31:20 +0530

    Pattern_13.py

--- Pattern/Pattern_13.py
+++ Pattern/Pattern_13.py
@@ -2,10 +2,14 @@ n = 6
 num =1
 for i in range (n):
     for j in range(n-i-1):
+        
         print(" ",end=" ")
     for k in range(2*i+1):
+       
         print(k+1,end=" ")
+   
     print() 
+    
 
 
 #         1 
