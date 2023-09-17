af0349f2e0fb4d8256194a9d92496ffeb0325c10
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-09-17 23:50:14 +0530

    .

---
+++ stacks/balancedparanthesis.py
@@ -0,0 +1,26 @@
+def isMatch(a,b):
+    if (a=="(" and b==")") or (a=="{"and b =="}") or  (a=="["and b=="]"):
+        return True
+    else:
+        return False
+def fun (string):
+    stack=[]
+    for x in string:
+        if x in ("(","[","{"):         #stack has these three
+            stack.append(x)
+        else:
+            if not stack:             #)() suppose this case stack is empty because start of the string is )
+                return False
+            elif isMatch(stack[-1],x)==False:
+                return False
+            else:
+                stack.pop()             #remove one bracket from  stack if matching is Found  
+    
+    
+    if stack:                        #if after the loop there is still any opening bracket left return false
+        return False
+    else:
+        return True         #else True
+
+string=input()
+print(fun(string))
