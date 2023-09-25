f73e66e298e1206efe6047d7cc535f3dbb3420e2
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-09-26 00:01:37 +0530

    Matrix

--- matrix/boundryTraverse.py
+++ matrix/boundryTraverse.py
@@ -1,7 +1,25 @@
-def traverse(matrix):
+def traverse(arr):
 
-    row = len(matrix)
-    col = len(matrix[0])
+    N = len(arr);
+    M = len(arr[0]);
+    for i in range(M):
+        print(arr[0][i], end = " ");
+
+    for i in range(1, N):
+        print(arr[i][M - 1], end = " ");
+ 
+
+    if (N > 1):
+ 
+        for i in range(M - 2, -1, -1):
+            print(arr[N - 1][i], end = " ");
+ 
+
+    if (M > 1):
+ 
+      
+        for i in range(N - 2, 0, -1):
+            print(arr[i][0], end = " ");
     
 
 matrix = [[1,2,3,4],
