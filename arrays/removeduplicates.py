163193f965179f49d5389341244dbcb98dfeca89
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-10-03 23:53:50 +0530

    .

--- arrays/removeduplicates.py
+++ arrays/removeduplicates.py
@@ -1,21 +1,14 @@
 def remove(l):
     res = 1 
-    for i in range(1 , len(l)):
-        if l[i] != l[res-1]:                 
-            l[res]=l[i]                      #moving the element to the place back  , where it is not duplicate with the previous one 
-            res+=1                          #incremented if element is not duplicate
-    return res                             #storing how many times we removed the element 
+    for i in range(1 , len(l)):     #copying the first element as it is .
+        if l[i] != l[i-1]:                 #checking the second element with the first element , if same copy it in the original array else do nothing
+            l[res]=l[i]
+            res+=1                           #increment res 
+    return res
+l = [1,1,1,2,2,3,3,3,3,3,3,3,3,3]
+res = remove(l)
+for i in range(res):
+    print(l[i],end="")
 
-def rem(l):
-    n = remove(l)
-    for i in range(n):
-        print( l[i] , end = ' ')
-l = []
-n = int(input())
-for i in range(0, n):
-    f = int(input())
-    l.append(f)
-rem(l)
+# dry run [  1 , 1  ,2 ,3  ]
 
-
-#dry run [  1 , 1 , 1 ,2 ]
