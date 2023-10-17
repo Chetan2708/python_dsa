7d1591cb08acad912976f732cceea5c908d9503f
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-10-17 23:53:05 +0530

    Initial commit

--- stacks/Reversestring.py
+++ stacks/Reversestring.py
@@ -1,11 +1,10 @@
 def fun(x):
     stack=[]
     st = ""
-
     for i in range(len(x)):
         stack.append(x[i])
     for i in range(len(stack)):
-        st+=(stack.pop())
+        st+=(stack.pop()) #popping the har from stack and adding it to st 
     print(st)
 
 x = input()
