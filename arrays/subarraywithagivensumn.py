4e9406f38009fa7305f4afa8a68e351c59d548b5
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-10-27 23:57:42 +0530

    Initial commit

--- arrays/subarraywithagivensumn.py
+++ arrays/subarraywithagivensumn.py
@@ -4,15 +4,14 @@ def subarray_with_sum(l, target_sum):
     result = []
 
     for i in range(len(l)):
-        current_sum += l[i]
-
-        # If the current sum exceeds the target sum, move the start index
+        current_sum += l[i]  # If the current sum exceeds the target sum, move the start index
         # and subtract the element at start index from the current sum
         while current_sum > target_sum:
             current_sum -= l[start]
             start += 1
 
         # If the current sum matches the target sum, return True
+        
         if current_sum == target_sum:
             return True
 
