49ad1a02bfe5db022ea2df2a0c4ee0d1e6b1aeb3
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2024-04-01 13:12:11 +0530

    Time: 110 ms (20.80%) | Memory: 17.6 MB (28.24%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,22 +1,36 @@
 class Solution:
-    def setZeroes(self, mat: List[List[int]]) -> None:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows = len(mat)
-        cols = len(mat[0])
+        n = matrix
+        m = matrix[0]
+        rowZero = True
 
-        rowArray = [0]* rows
-        colsArray = [0]* cols
-
-        for i in range(rows):
-            for j in range(cols):
-                if not mat[i][j]:  #If zero is found
-                    rowArray[i] = 1
-                    colsArray[j] =1
+        # Traverse and mark first row and first column 
+        for i in range(len(n)):
+            for j in range(len(m)):
+                if matrix[i][j]== 0 :
+                    matrix[0][j] = 0 
                 
-        for i in range(rows):
-            for j in range(cols):
-                if rowArray[i] or colsArray[j]:
-                    mat[i][j] = 0
+                    if i > 0:
+                        matrix[i][0] = 0
+                    else:
+                        rowZero = False
+        # mark the the whole matrix according to first row and column set earlier .. Start from 1st row and column because 1 box has already defined for zeroth row
+    
+        for i in range(1, len(n)):
+            for j in range(1 , len(m)):
+                if matrix[0][j] == 0 or matrix[i][0]  == 0:
+                    matrix[i][j]= 0
+
+# If 1st is zero then all rows = 0 
+        if matrix[0][0] == 0 :
+            for i in range(len(n)):
+                matrix[i][0] = 0 
+ # If 1st is zero then all cols = 0        
+        if not rowZero :
+            for j in range(len(m)):
+                matrix[0][j] = 0 
+    
 
