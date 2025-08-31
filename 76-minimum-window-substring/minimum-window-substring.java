728f65008598679aeb187629bfa50e3db50cdf80
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2025-08-31 12:44:30 +0530

    Time: 14 ms (72.12%) | Memory: 45.1 MB (60.38%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public String minWindow(String s, String t) {
         if (s.length() < t.length()) return "";
         int n = s.length();
@@ -12,23 +12,23 @@ class Solution {
         }
 
         int i = 0, j = 0;
-        int formed = t.length();   // total chars we still need
+        int count = 0;   // how many chars we matched
         int minLen = Integer.MAX_VALUE;
         int start = 0;
 
         while (j < n) {
             char ch = s.charAt(j);
 
-            // if this char was still needed, reduce formed
+            // if this char is still needed, increase count
             if (need.getOrDefault(ch, 0) > 0) {
-                formed--;
+                count++;
             }
 
-            // decrement count in need map
+            // decrement count in need map (even if not needed, we track surplus)
             need.put(ch, need.getOrDefault(ch, 0) - 1);
 
             // try shrinking from left while valid
-            while (formed == 0) {
+            while (count == t.length()) {
                 if (minLen > j - i + 1) {
                     minLen = j - i + 1;
                     start = i;
@@ -37,9 +37,9 @@ class Solution {
                 char left = s.charAt(i);
                 need.put(left, need.getOrDefault(left, 0) + 1);
 
-                // if we removed a required char, window invalid again
+                // if removing left breaks requirement, reduce count
                 if (need.get(left) > 0) {
-                    formed++;
+                    count--;
                 }
                 i++;
             }
